f2624564502b135eadb40f8273d8126f5725f68f
Gnz03 <gonzalomoralessegura@gmail.com>
2024-07-16 20:45:02 -0500

    Flujo de login

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.set('views', path.join(__dirname, 'views'));
 // Configurar Express para servir archivos estáticos desde la carpeta 'views'
 app.use(express.static('views'));
 
+// Servir archivos estáticos desde la carpeta 'img' para imágenes
+app.use('/img', express.static(path.join(__dirname, 'img')));
+
 // Configurar Express para servir archivos estáticos desde la carpeta 'styles' para CSS
 app.use('/styles', express.static(path.join(__dirname, 'styles')));
 // Middleware para analizar datos de formulario en el cuerpo de las solicitudes
@@ -23,12 +26,18 @@ const config = {
     user: 'admin',
     password: '1234'
 };
-const pool = new mssql.ConnectionPool(config);
+const poolPromise = mssql.connect(config);
 
-// Crear la conexión a la base de datos
-const connection = new mssql.ConnectionPool(config);
+
 // Middleware para parsear el cuerpo de las solicitudes como JSON
 app.use(express.json()); // Usar express.json() para analizar los datos JSON
+
+
+// Ruta para servir admin.html protegida por el middleware isAdmin
+app.get('/admin', isAdmin, (req, res) => {
+    // Aquí servirías tu página admin.html
+    res.sendFile(path.join(__dirname, 'ruta/a/tu/admin.html'));
+});
 
 
 // Ruta para actualizar el nivel de usuario
@@ -61,7 +70,7 @@ app.get('/modulo-presupuesto', (req, res) => {
 async function actualizarNivelUsuario(userId, nuevoNivel) {
     try {
        
-        const pool = await mssql.connect(config);
+        const pool = await poolPromise;
 
         const request = pool.request();
         request.input('userId', mssql.Int, userId);
@@ -77,18 +86,21 @@ async function actualizarNivelUsuario(userId, nuevoNivel) {
         throw error;
     }
 }
-
 // Ruta para establecer el nivel del usuario
 app.post('/establecer-nivel', async (req, res) => {
-    console.log(req.body); // Agrega este console.log para depurar
     const { userId, option } = req.body;
+
+    if (!userId || !option) {
+        return res.status(400).send('Datos incompletos o incorrectos');
+    }
 
     try {
         if (option === 'basico') {
             const nuevoNivel = 'basico';
+           
             const result = await actualizarNivelUsuario(userId, nuevoNivel);
             if (result) {
-                res.redirect('/modulo-presupuesto.html');
+                res.json({ success: true });
             } else {
                 res.status(500).send('Error al actualizar el nivel de usuario');
             }
@@ -123,7 +135,7 @@ app.post('/login', async (req, res) => {
 async function loginUsuario(email, password) {
     try {
         // Conectar con la base de datos
-        const pool = await mssql.connect(config);
+        const pool = await poolPromise;
 
         // Ejecutar el procedimiento almacenado usp_LoginUsuario
         const request = pool.request();
@@ -158,6 +170,107 @@ async function loginUsuario(email, password) {
         throw error;
     }
 }
+
+// Ruta para registrar un nuevo usuario
+app.post('/registrar', async (req, res) => {
+    const { name, email, password } = req.body;
+
+    console.log('Datos recibidos en /registrar:', { name, email, password });
+
+    if (!name || !email || !password) {
+        return res.status(400).json({ success: false, message: 'Todos los campos son obligatorios' });
+    }
+
+    try {
+        const pool = await poolPromise;
+        const request = pool.request();
+        request.input('vNombreUsuario', mssql.VarChar(100), name);
+        request.input('vEmailUsuario', mssql.VarChar(100), email);
+        request.input('vPasswordUsuario', mssql.VarChar(100), password);
+
+        const result = await request.execute('usp_RegistrarUsuario');
+
+        if (result.returnValue === 0) {
+            res.json('Usuario registrado con éxito'); // Envía solo el mensaje de éxito
+        } else if (result.returnValue === -1) {
+            res.status(400).json({ success: false, message: 'El usuario ya existe' });
+        } else {
+            res.status(500).json({ success: false, message: 'Error al registrar el usuario' });
+        }
+    } catch (error) {
+        console.error('Error al registrar el usuario:', error);
+        res.status(500).json({ success: false, message: 'Error interno del servidor' });
+    }
+});
+
+// Ruta para obtener el número total de usuarios
+app.get('/admin/total-usuarios', async (req, res) => {
+    try {
+        const pool = await poolPromise;
+        const result = await pool.request().execute('sp_NumeroTotalUsuarios');
+        res.json(result.recordset);
+    } catch (error) {
+        console.error('Error al obtener el número total de usuarios:', error);
+        res.status(500).send('Error interno del servidor');
+    }
+});
+
+// Ruta para obtener el número de usuarios por nivel
+app.get('/admin/usuarios-por-nivel', async (req, res) => {
+    try {
+        const pool = await poolPromise;
+        const result = await pool.request().execute('sp_UsuariosPorNivel');
+        console.log('Resultado del procedimiento sp_numerototalusuarios:', result); // Línea de depuración
+        res.json(result.recordset);
+    } catch (error) {
+        console.error('Error al obtener usuarios por nivel:', error);
+        res.status(500).send('Error interno del servidor');
+    }
+});
+
+// Ruta para obtener el número de usuarios registrados por fecha
+app.get('/admin/usuarios-por-fecha', async (req, res) => {
+    try {
+        const pool = await poolPromise;
+        const result = await pool.request().execute('UsuariosRegistradosPorFecha');
+        res.json(result.recordset);
+    } catch (error) {
+        console.error('Error al obtener usuarios registrados por fecha:', error);
+        res.status(500).send('Error interno del servidor');
+    }
+});
+
+// Ruta para obtener usuarios registrados por rango de fecha
+app.get('/admin/usuarios-por-rango-fecha', async (req, res) => {
+    const { fechaInicio, fechaFin } = req.query;
+
+    try {
+        const pool = await poolPromise;
+        const request = pool.request();
+        request.input('FechaInicio', mssql.Date, fechaInicio);
+        request.input('FechaFin', mssql.Date, fechaFin);
+        const result = await request.execute('UsuariosRegistradosPorRangoFecha');
+
+        res.json(result.recordset);
+    } catch (error) {
+        console.error('Error al obtener usuarios registrados por rango de fecha:', error);
+        res.status(500).send('Error interno del servidor');
+    }
+    
+});
+
+// Middleware para verificar el rol de administrador
+function isAdmin(req, res, next) {
+    // Aquí deberías tener lógica para verificar si el usuario tiene el rol de administrador
+    const user = req.user; // Suponiendo que el usuario está en el objeto de solicitud, dependiendo de cómo manejes la autenticación
+
+    if (user && user.role === 'ADMIN') {
+        next(); // Permitir el acceso al siguiente middleware o ruta
+    } else {
+        res.status(403).send('Acceso denegado'); // Si no tiene el rol adecuado, devolver un error 403 Forbidden
+    }
+}
+
 const port = 3000;
 // Iniciar el servidor
 app.listen(port, () => {
